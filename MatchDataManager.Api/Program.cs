b0e1d14c32c009a77c1f84aef977062ff187988a
Adriana Barteczko <a.barteczko@po.edu.pl>
2022-06-11 03:08:07 +0200

    Added Database setup

--- MatchDataManager.Api/Program.cs
+++ MatchDataManager.Api/Program.cs
@@ -1,8 +1,16 @@
+using System.Configuration;
+using MatchDataManager.Api;
+using MatchDataManager.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
+builder.Services.AddDbContext<MatchDataManagerDbContext>(options => options.UseMySQL(builder.Configuration.GetConnectionString("MatchDataManagerDbContext")));
+builder.Services.AddTransient<DatabaseSeed>();
 builder.Services.AddControllers();
+builder.Services.AddScoped<ILocationsRepository, LocationsRepository>();
+// builder.Services.AddScoped<ITeamsRepository, TeamsRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,6 +22,15 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+using (var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+{
+    var context = serviceScope.ServiceProvider.GetRequiredService<MatchDataManagerDbContext>();
+    var databaseSeed = serviceScope.ServiceProvider.GetRequiredService<DatabaseSeed>();
+
+    context.Database.EnsureDeleted();
+    context.Database.EnsureCreated();
+    databaseSeed.Seed();
 }
 
 app.UseHttpsRedirection();
